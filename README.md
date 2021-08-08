9c8ad5ad1236be47b4cf590df5cb23d9e79b7292
super-okay <wangallen98@gmail.com>
2021-08-08 16:55:08 -0500

    added readme, updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello My Good Sir !
+
+If somebody mentions me on Twitter, this bot will automatically respond with "Hello my good sir !".
--- filtered_stream.py
+++ filtered_stream.py
@@ -1,3 +1,7 @@
+import requests
+import os
+import json
+
 from secret_stuff import (
     BEARER_TOKEN,
     API_KEY,
@@ -25,8 +29,10 @@ def get_rules():
         raise Exception(
             "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
         )
+    print("\n---------- GETTING RULES ----------\n")
     print(json.dumps(response.json(), indent=4))
     return response.json()
+
 
 def set_rules():
     """
@@ -45,9 +51,38 @@ def set_rules():
         raise Exception(
             "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
         )
+    print("\n---------- SETTING RULES ----------\n")
+    print(json.dumps(response.json(), indent=4))
+
+
+def delete_all_rules(rules):
+    """
+        deletes all set rules
+    """
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    print("\n---------- DELETING ALL RULES ----------\n")
     print(json.dumps(response.json(), indent=4))
 
 
 if __name__ == "__main__":
     set_rules = set_rules()
     rules = get_rules()
+
+    ##### DELETES ALL RULES #####
+    # rules = get_rules()
+    # delete_all_rules(rules)
